07c93bc024b68bc5df37d3d22c80adbe35f732fb
Alexander Richards <a.richards@imperial.ac.uk>
2023-05-12 14:34:01 +0100

    Updating tests.

---
+++ tests/test_coffeecake.py
@@ -0,0 +1,58 @@
+from importlib import import_module
+import pytest
+
+@pytest.fixture(scope="module")
+def cc():
+    return import_module("coffeecake")
+
+class TestTask11:
+    def test_module_exists(self, cc):
+        pass
+    def test_var_a(self, cc):
+        assert hasattr(cc, "a")
+    def test_a_val(self, cc):
+        assert cc.a == "coffee"
+    def test_var_b(self, cc):
+        assert hasattr(cc, "b")
+    def test_b_val(self, cc):
+        assert cc.b == "cake"        
+    def test_func1_exists(self, cc):
+        assert hasattr(cc, "func1")
+    def test_func1_output(self, cc):
+        assert cc.func1() == ("drunk", "eaten")
+    def test_func2_exists(self, cc):
+        assert hasattr(cc, "func2")
+    def test_func2_output(self, cc):
+        assert cc.func2(1, 2) == ("drunk", "eaten")
+    def test_func3_exists(self, cc):
+        assert hasattr(cc, "func3")
+    def test_func3_output(self, cc):
+        assert cc.func3() == "coffee"
+        assert cc.func3() is cc.a
+    def test_func4_exists(self, cc):
+        assert hasattr(cc, "func4")
+    def test_func5_exists(self, cc):
+        assert hasattr(cc, "func5")
+    def test_func5_output(self, cc):
+        a, b = cc.func5()
+        assert (a, b) == ("drunk", "eaten")
+        assert a is cc.a
+
+class TestTask12:
+    def test_func6_exists(self, cc):
+        assert hasattr(cc, "func6")
+
+    def test_func6_output_type(self, cc):
+        assert isinstance(cc.func6(), dict)
+
+    def test_func6_output(self, cc):
+        assert cc.func6()['a'] == "drunk"
+
+    def test_func7_exists(self, cc):
+        assert hasattr(cc, "func7")
+
+    def test_func7_output_type(self, cc):
+        assert isinstance(cc.func7(), dict)
+
+    def test_func7_output(self, cc):
+        assert cc.func7()['a'] != "empty"
---
+++ tests/test_mymodule.py
@@ -0,0 +1,33 @@
+from importlib import import_module
+import pytest
+import numpy as np
+
+@pytest.fixture(scope="module")
+def mm():
+    return import_module("mymodule")
+
+class TestTask8:
+    def test_module_exists(self, mm):
+        pass
+
+class TestTask9:
+    def test_import_np(self, mm):
+        assert hasattr(mm, "_np")
+
+    def test_fibo_exists(self, mm):
+        assert hasattr(mm, "fibo")
+
+    def test_fibo_output(self, mm):
+        assert np.isclose(mm.fibo(5), 5)
+
+    def test_fibo1_exists(self, mm):
+        assert hasattr(mm, "fibo1")
+
+    def test_fibo1_output(self, mm):
+        assert np.isclose(mm.fibo1(5), 5)
+
+    def test_fibo2_exists(self, mm):
+        assert hasattr(mm, "fibo2")
+
+    def test_fibo2_output(self, mm):
+        assert np.isclose(mm.fibo2(5), 5)
