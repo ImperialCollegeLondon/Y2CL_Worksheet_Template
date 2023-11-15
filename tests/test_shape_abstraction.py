6c59788890c3f8b60fc86c18c958b07bcfb93088
Alexander Richards <a.richards@imperial.ac.uk>
2023-11-15 11:42:52 +0000

    updates

--- tests/test_shape_abstraction.py
+++ tests/test_shape_abstraction.py
@@ -1,4 +1,86 @@
 
+import inspect as ins
+from importlib import import_module, reload
+from unittest.mock import MagicMock
+import numpy as np
+
 class TestTask29:
     def test_module_exists(self, sh_ab):
         pass
+
+    def test_shape_in_scope(self, sh_ab):
+        assert "Shape" in vars(sh_ab)
+
+    def test_not_reimplemented(self, sh, sh_ab, monkeypatch):
+
+        shape_mock = MagicMock()
+        with monkeypatch.context() as mpc:
+            mpc.setattr(sh, "Shape", shape_mock)
+            reload(sh_ab)
+            assert sh_ab.Shape is shape_mock
+        reload(sh_ab)
+
+
+class TestTask30:
+    def test_regularpolygon_exists(self, sh_ab):
+        assert "RegularPolygon" in vars(sh_ab)
+
+    def test_construction(self, sh_ab):
+        sh_ab.RegularPolygon(n_sides=4, side_length=3)
+        sh_ab.RegularPolygon(n_sides=3, side_length=4, colour="blue")
+
+    def test_area_present(self, sh_ab):
+        assert "area" in vars(sh_ab.RegularPolygon)
+
+    def test_area_calculation(self, sh_ab):
+        assert np.isclose(sh_ab.RegularPolygon(n_sides=3, side_length=5).area(), 10.825317547305486)
+        assert np.isclose(sh_ab.RegularPolygon(n_sides=4, side_length=5).area(), 25.)
+        assert np.isclose(sh_ab.RegularPolygon(n_sides=5, side_length=5).area(), 43.01193501472418)
+
+
+class TestTask31:
+    def test_eqtriangle_exists(self, sh_ab):
+        assert "EquilateralTriangle" in vars(sh_ab)
+
+    def test_eqtriangle_construction(self, sh_ab):
+        sh_ab.EquilateralTriangle(side_length=5)
+        sh_ab.EquilateralTriangle(side_length=5, colour="yellow")
+
+    def test_square_exists(self, sh_ab):
+        assert "Square" in vars(sh_ab)
+
+    def test_square_construction(self, sh_ab):
+        sh_ab.Square(side_length=5)
+        sh_ab.Square(side_length=5, colour="magenta")
+
+    def test_pentagon_exists(self, sh_ab):
+        assert "Pentagon" in vars(sh_ab)
+    
+    def test_pentagon_construction(self, sh_ab):
+        sh_ab.Pentagon(side_length=5)
+        sh_ab.Pentagon(side_length=5, colour="black")
+
+
+class TestTask32:
+    def test_angles_method_exists(self, sh_ab):
+        assert "interior_angle" in vars(sh_ab.RegularPolygon)
+
+    def test_interior_angle(self, sh_ab):
+        assert np.isclose(sh_ab.EquilateralTriangle(side_length=5).interior_angle(), 60.)
+        assert np.isclose(sh_ab.Square(side_length=5).interior_angle(), 90.)
+        assert np.isclose(sh_ab.Pentagon(side_length=5).interior_angle(), 108.)
+
+    def test_tessellate_exists(self, sh_ab):
+        assert ("can_tessellate" in vars(sh_ab.RegularPolygon)) or \
+            ("can_tessellate" in vars(sh_ab.EquilateralTriangle) and
+             "can_tessellate" in vars(sh_ab.Square) and
+             "can_tessellate" in vars(sh_ab.Pentagon))
+        assert ("can_tessellate" in vars(sh_ab.RegularPolygon)) != \
+            ("can_tessellate" in vars(sh_ab.EquilateralTriangle) or
+             "can_tessellate" in vars(sh_ab.Square) or 
+             "can_tessellate" in vars(sh_ab.Pentagon)), "can_tessellate should not be in both base and derived classes."
+
+    def test_can_tessellate(self, sh_ab):
+        assert sh_ab.EquilateralTriangle(side_length=5).can_tessellate()
+        assert sh_ab.Square(side_length=5).can_tessellate()
+        assert not sh_ab.Pentagon(side_length=5).can_tessellate()
