60bb551a65b47ef97959c4df87eac0581f19c4eb
Alexander Richards <inferno_alex@hotmail.com>
2024-03-27 13:29:17 +0000

    Removing old setup scripts.

--- setup-linux.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-python3 -m venv venv
-venv/bin/python -m pip install --upgrade pip setuptools wheel
-venv/bin/python -m pip install -e .
-venv/bin/python .setup-post.py
--- setup-windows.ps1
+++
@@ -1,4 +0,0 @@
-python3 -m venv venv
-venv/Scripts/python -m pip install --upgrade pip setuptools wheel
-venv/Scripts/python -m pip install -e .
-venv/Scripts/python .\.setup-post.py
