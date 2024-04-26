18357b59ce02af25673747660350723bfa23cbb2
Alexander Richards <inferno_alex@hotmail.com>
2024-04-26 13:17:23 +0100

    updates

--- tests/test_relativity.py
+++ tests/test_relativity.py
@@ -6,7 +6,7 @@ import numpy as np
 
 @pytest.fixture(scope="module")
 def rel():
-    return import_module("relativity")
+    return import_module("src.relativity")
 
 @pytest.fixture
 def fv_namespace(rel):
