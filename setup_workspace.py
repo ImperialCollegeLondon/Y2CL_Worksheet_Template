994d06cf70c6810bf954bdb31c989ecee133acfc
Alexander Richards <a.richards@imperial.ac.uk>
2025-03-19 14:20:09 +0000

    Adding new environment setup script

--- setup_workspace.py
+++ setup_workspace.py
@@ -2,9 +2,7 @@
 Setup Script.
 
 Use this script to cerate the user.email and user.name entries necessary in .gitconfig
-This script will also set up a virtual environment from the global distribution and
-install all needed dependencies. (This is unfortunately necessary as the global distribution
-is not on the path for new terminals)
+This script will also link the current project into your Python environment.
 
 NOTE: this script is intended to be run on the level 3 windows computers. Having said that it
 should work on your personal computers (regardless of OS) but has not been tested on an OS other
@@ -18,6 +16,7 @@ from pathlib import Path
 from subprocess import run
 logging.basicConfig(level=logging.INFO, format="%(levelname)-7s : %(message)s")
 
+
 logging.info("Starting script...")
 
 logging.info("Detecting platform...")
@@ -30,24 +29,11 @@ logging.info("Found operating system: %s", 'MacOS' if SYSTEM == 'Darwin' else SY
 logging.info("GitHub information required")
 name = input("Enter your name: ")
 email = input("Enter the email address registered with GitHub: ")
-run(['git', 'config', '--global', 'user.email', email], check=False, stdout=sys.stdout, stderr=sys.stderr)
 run(['git', 'config', '--global', 'user.name', name], check=False, stdout=sys.stdout, stderr=sys.stderr)
+run(['git', 'config', '--global', 'user.email', email], check=False, stdout=sys.stdout, stderr=sys.stderr)
 
-
-logging.info("Creating (and updating) new python virtual environment...")
-run([sys.executable, "-m", "venv", "venv"], check=False, stdout=sys.stdout, stderr=sys.stderr)
-
-VENV_PATH = Path('venv')
-if SYSTEM == "Windows":
-    VENV_INTERPRETER = VENV_PATH.joinpath('Scripts', 'python.exe')
-else:
-    VENV_INTERPRETER = VENV_PATH.joinpath('bin', 'python')
-logging.info("Using virtual env interpreter: %s", VENV_INTERPRETER)
-run([VENV_INTERPRETER, '-m', 'pip', 'install', '--upgrade', 'pip', 'setuptools', 'wheel'],
-    check=False, stdout=sys.stdout, stderr=sys.stderr)
-
-logging.info("Installing dependencies...")
-run([VENV_INTERPRETER, '-m', 'pip', 'install', '-e', '.'], check=False, stdout=sys.stdout, stderr=sys.stderr)
+logging.info("Linking current project into Python environment...")
+run([sys.executable, "-m", "pip", "install", "--user", "-e", "."], check=False, stdout=sys.stdout, stderr=sys.stderr)
 
 logging.info("Setup complete!")
 
